6e4997da3a6c3ed4c409c786ed11f55550f69a27
rafael dos anjos <rafa.areas@hotmail.com>
2020-08-07 04:00:59 -0300

    alteraçao do perfil

--- src/server.js
+++ src/server.js
@@ -5,19 +5,19 @@ const proffys = [
     whatsapp:"89987654634",
     bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
     subject:"Química", 
-    cost:"20", 
+    cost:"20,00", 
     weekday:[0], 
     time_from:[720], 
     time_to:[1220]
 },
 {
-    name:"daniele ", 
+    name:"Rafael dos Anjos ", 
     avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
     whatsapp:"89987654634",
-    bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    bio:"Física não é somente uma matéria, ela é um complemento da sua vida. Professor totalmente engajado em fazer o aluno ficar mais confuso do que quando entrou na aula, e também impressionado com a genialidade da vida e do universo universo.",
     subject:"Química", 
-    cost:"20", 
-    weekday:[1], 
+    cost:"300,00", 
+    weekday:[4], 
     time_from:[720], 
     time_to:[1220]
 }
