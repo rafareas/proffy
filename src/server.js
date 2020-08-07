3f795830cb1b97570e69c11152c5f1b7f00929fa
rafael dos anjos <rafa.areas@hotmail.com>
2020-08-07 04:02:47 -0300

    alteração perfil

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const proffys = [
 },
 {
     name:"Rafael dos Anjos ", 
-    avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+    avatar:"https://scontent.fcfb2-1.fna.fbcdn.net/v/t31.0-8/20229450_1444277112329874_8421907480401079454_o.jpg?_nc_cat=107&_nc_sid=09cbfe&_nc_ohc=lZRWSX8RqPkAX9bYAyX&_nc_ht=scontent.fcfb2-1.fna&oh=f13f099bd201634ad9466be871053e3c&oe=5F522E29",
     whatsapp:"89987654634",
     bio:"Física não é somente uma matéria, ela é um complemento da sua vida. Professor totalmente engajado em fazer o aluno ficar mais confuso do que quando entrou na aula, e também impressionado com a genialidade da vida e do universo universo.",
     subject:"Química", 
